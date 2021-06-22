f9c09802dc37e2edfa26b78dbf8735edaf4070e1
Maycon Pires <59845199+MayconPires-exe@users.noreply.github.com>
2021-06-21 22:20:43 -0300

    install_sqlite
    preparação para o banco

--- nlwvaloriza/src/server.ts
+++ nlwvaloriza/src/server.ts
@@ -3,21 +3,12 @@ import express, { response } from "express";
 
 const app = express();
 
-/**
- * GET   => Buscar uma informação, "qualquer" busca
- * POST  => Inserir (Criar)uma informação
- * PUT   => Alteração ou manipulação de usuario, nome, email, etc
- * DELET => Remover alguma informação
- * PATCH => Alterar uma informação específica. ex:senha
- */
-
-
 app.get("/test", (req, res) => {
-    return res.send("Olá método GET");
+	return res.send("Olá método GET");
 });
 
 app.post("/test-post", (req, res) => {
-    return res.send("Olá método POST");
+	return res.send("Olá método POST");
 });
 
 // http://localhost:3000
