db0d074fffb6883345446d07bf9878fa47ae7840
Maycon Pires <59845199+MayconPires-exe@users.noreply.github.com>
2021-06-27 10:15:38 -0300

    Correção de escrita

--- nlwvaloriza/src/repositories/ComplimentsTagsRepositories.ts
+++
@@ -1,9 +0,0 @@
-import { Repository, EntityRepository } from "typeorm";
-import { Compliment } from "../entities/Compliment";
-
-@EntityRepository(Compliment)
-class ComplimentsTagsRepositories extends Repository<Compliment> {
-
-}
-
-export { ComplimentsTagsRepositories };
