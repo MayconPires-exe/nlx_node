0464b105e5c0ce432e7a881e6a95e9a25667d43f
Maycon Pires <59845199+MayconPires-exe@users.noreply.github.com>
2021-06-27 10:16:00 -0300

    Create Compliment

--- nlwvaloriza/src/services/CreateComplimentService.ts
+++ nlwvaloriza/src/services/CreateComplimentService.ts
@@ -1,12 +1,18 @@
+import { getCustomRepository } from "typeorm"
+import { ComplimentsRepositories } from "../repositories/ComplimentsRepositories"
 
 
-
-interface
+interface ICreateComplimentRequest {
+    tag_id: string;
+    user_sender: string;
+    user_receiver: string;
+    message: string;
+}
 
 class CreateComplimentService {
 
-    async execute({ }) {
-
+    async execute({ tag_id, user_receiver, user_sender, message }: ICreateComplimentRequest) {
+        const ComplimentsRepositories = getCustomRepository(ComplimentsRepositories);
     }
 }
 
